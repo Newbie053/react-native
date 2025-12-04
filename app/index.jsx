2de8c11e0ce969b181ce260ea6e36dbf53816674
newbie053 <dipto30901@gmail.com>
2025-12-04 15:15:12 +0600

    wip:file base navigation lec 3

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 //import { Container } from 'lucide-react'
 import Logo from '../assets/img/logo.png'
+import { Link } from 'expo-router'
 
 const Home = () => {
     return (
@@ -10,9 +11,11 @@ const Home = () => {
 
             <Text style={styles.title}>The Number 1</Text>
             <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
-            <View>
+            {/* <View>
                 <Text style={styles.card}>Hello,this is a card.</Text>
-            </View>
+            </View> */}
+            <Link href="/about" style={styles.link}>About Page</Link>
+            <Link href="/contact" style={styles.link}>Contact Page</Link>
         </View>
     )
 }
@@ -40,4 +43,8 @@ const styles = StyleSheet.create({
     img: {
         marginVertical: 20
     },
+    link: {
+        marginVertical: 10,
+        borderBottomWidth: 1
+    }
 })
