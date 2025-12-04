f0a0bcdbf13b5fedc5f2fc558dd45944317d9f9e
newbie053 <dipto30901@gmail.com>
2025-12-04 15:02:39 +0600

    wip:text view image

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+//import { Container } from 'lucide-react'
+import Logo from '../assets/img/logo.png'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.Container}>
+            <Image style={styles.img} source={Logo} />
+
+            <Text style={styles.title}>The Number 1</Text>
+            <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+            <View>
+                <Text style={styles.card}>Hello,this is a card.</Text>
+            </View>
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: "4px 4px rgba(0,0,0,0.1)"
+    },
+    img: {
+        marginVertical: 20
+    },
+})
